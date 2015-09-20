9a1d92278f59277eef7cb724d4cf4a5123d0892f
takahashi-shinobu <rallys1122@yahoo.co.jp>
2015-09-21 03:33:46 +0900

    sql log off

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,9 +16,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\SomeEvent' => [
             'App\Listeners\EventListener',
         ],
-        'illuminate.query' => [
-            'App\Handlers\Events\QueryLogTracker',
-        ],
+//        'illuminate.query' => [
+//            'App\Handlers\Events\QueryLogTracker',
+//        ],
     ];
 
     /**

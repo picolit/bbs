8aad2fffd727ba07e1da6f22b3923bb099f72d60
takahashi-shinobu <rallys1122@yahoo.co.jp>
2015-11-16 23:16:53 +0900

    楽店アフェリエイト

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -72,16 +72,5 @@
 @show
 {{-- 解説: セクションをこの場所に展開させたい場合、@showを指定します。--}}
     </script>
-
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- GoogleAdSense -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:300px;height:600px"
-     data-ad-client="ca-pub-5996210577415830"
-     data-ad-slot="4443757301"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
 </body>
 </html>
--- resources/views/parts/left_content.blade.php
+++ resources/views/parts/left_content.blade.php
@@ -67,4 +67,8 @@
             </ul>
         </div>
     </div>
+
+    <div>
+        <!-- Rakuten Widget FROM HERE --><script type="text/javascript">rakuten_design="slide";rakuten_affiliateId="13ed97bb.2bd40133.13ed97bc.3f8c0543";rakuten_items="ranking";rakuten_genreId=0;rakuten_size="300x250";rakuten_target="_blank";rakuten_theme="gray";rakuten_border="on";rakuten_auto_mode="on";rakuten_genre_title="off";rakuten_recommend="on";</script><script type="text/javascript" src="http://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script><!-- Rakuten Widget TO HERE -->
+    </div>
 </div>
